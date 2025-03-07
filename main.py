a1cb664d22a9d08f0a108cd5ba0a74af758fd5ad
Groom, Tyler <tylergroom@proton.me>
2025-03-07 09:26:16 -0600

    changed favorite food

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # This is a program that will print your favorite food to the console!
 
 # Make changes to the line below to change the printed message
-my_favorite_food = 'Pizza'
+my_favorite_food = 'Sushi'
 
 # Print out my_favorite_food to the console
 print(f'My favorite is {my_favorite_food}!')
